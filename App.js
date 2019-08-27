5a5b141450e5090e80bb0e082202111be63c4713
ataricoder <ataricoder@gmail.com>
2019-08-27 14:35:48 -0500

    added array data

--- App.js
+++ App.js
@@ -52,7 +52,18 @@ export default function App() {
             ))}
           </ScrollView>
           <Subtitle>Popular Courses</Subtitle>
-          <Course />
+          {courses.map((course, index) => (
+            <Course
+              key={index}
+              image={course.image}
+              title={course.title}
+              subtitle={course.subtitle}
+              logo={course.logo}
+              author={course.author}
+              avatar={course.avatar}
+              caption={course.caption}
+            />
+          ))}
         </ScrollView>
       </SafeAreaView>
     </Container>
