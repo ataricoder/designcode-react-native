0ae52bb8e7017592035d1de46b0f6ea7ed92bf99
ataricoder <ataricoder@gmail.com>
2019-09-04 18:34:20 -0500

    enabled push from card to section screen

--- navigator/AppNavigator.js
+++ navigator/AppNavigator.js
@@ -3,9 +3,14 @@ import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "../screens/HomeScreen";
 import SectionScreen from "../screens/SectionScreen";
 
-const AppNavigator = createStackNavigator({
-  Home: HomeScreen,
-  Section: SectionScreen
-});
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Section: SectionScreen
+  },
+  {
+    mode: "modal"
+  }
+);
 
 export default createAppContainer(AppNavigator);
